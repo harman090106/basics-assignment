23a09ed642dac5540236721638ecb897c130f161
harman090106 <145844415+harman090106@users.noreply.github.com>
2024-02-22 20:31:20 +0530

    Solved assignment files are added

---
+++ Basics/q1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //q1.
+    cout<<"Physics"<<endl;
+    cout<<"Wallah";
+}
---
+++ Basics/q5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+    //q5.
+    int radius=10;
+    int circumfrence=2*3.14*10;
+    cout<<circumfrence;
+}
